3905e1f432ca8ce6085ebcbd879ec95b5990ec09
TallManCycles <Aaronday@live.com.au>
2022-12-23 09:32:31 +1000

    Tried to add auth

--- App.js
+++ App.js
@@ -1,10 +1,26 @@
 import * as React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import { StyleSheet } from 'react-native';
+import { StyleSheet, Alert } from 'react-native';
 import Form from './Form';
 import CheckIn from './CheckIn';
 import Home from './Home'
+
+import * as firebase from "firebase/app";
+import * as auth from 'firebase/auth'
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDSK8LUhzod8aACyv7QEvX6EWDc_KTYGPk",
+  authDomain: "nutricoachme-native.firebaseapp.com",
+  databaseURL: "https://nutricoachme-native-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "nutricoachme-native",
+  storageBucket: "nutricoachme-native.appspot.com",
+  messagingSenderId: "57367713349",
+  appId: "1:57367713349:web:90285454aac96e9087f52c",
+  measurementId: "G-65JRMLDDLE"
+};
+
+let app = firebase.initializeApp(firebaseConfig);
 
 const Stack = createNativeStackNavigator();
 
@@ -17,6 +33,7 @@ export default function App() {
         <Stack.Screen name='Check In' component={CheckIn} />
       </Stack.Navigator>
     </NavigationContainer>
+
   );
 }
 
